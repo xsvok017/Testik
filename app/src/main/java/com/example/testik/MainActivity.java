89dd2030ad4ca2c9d3e0f44f377d82b6b800ab0a
KS <k.svobodova96@gmail.com>
2023-03-09 22:39:52 +0100

    přidáno datumítko nové přivítání

--- app/src/main/java/com/example/testik/MainActivity.java
+++ app/src/main/java/com/example/testik/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.testik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView labelDatum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        labelDatum = findViewById(R.id.twDatum);
+        Date c = Calendar.getInstance().getTime();
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+
+        labelDatum.setText(formattedDate);
+
+        //System.out.println("Current time => " + c);
     }
 }
