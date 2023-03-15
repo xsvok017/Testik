e7e056a18713789ad69a001fc405d7dfc5f5583c
Kacka <k.svobodova96@gmail.com>
2023-03-15 01:14:55 +0100

    přidané bottom menu + fragmenty, ale neumím opravit chyby v MainActivity.java, někde chybí id=container T_T

--- app/src/main/java/com/example/testik/MainActivity.java
+++ app/src/main/java/com/example/testik/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.testik;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.testik.*;
+
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     TextView labelDatum;
     @Override
@@ -25,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
 
         labelDatum.setText(formattedDate);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.vse);
+
         //System.out.println("Current time => " + c);
+    }
+    FragmentVse fragmentVse = new FragmentVse();
+    FragmentObj fragmentObj = new FragmentObj();
+    MapsFragment mapsFragment = new MapsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.vse:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentVse).commit();
+                return true;
+
+            case R.id.objednavka:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentObj).commit();
+                return true;
+
+            case R.id.mapa:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, mapsFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
